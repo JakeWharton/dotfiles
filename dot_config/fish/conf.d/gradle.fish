92faa87c9fa721e864538ed23cd5ff595e005d89
Jake Wharton <github@jakewharton.com>
2025-10-03 12:13:53 -0400

    Fail if Gradle wrapper not found

--- dot_config/fish/conf.d/gradle.fish
+++ dot_config/fish/conf.d/gradle.fish
@@ -2,6 +2,7 @@ function gw --wraps=gradle
   set -l GW "$(upfind gradlew)"
   if [ -z "$GW" ]
     echo "Gradle wrapper not found."
+    return 1
   else if contains -- "-p" $argv
     $GW --profile --parallel $argv
   else
